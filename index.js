02755acadba93061261e6303c3c1ee69957c50e7
“leonshimizu” <lmshimizu@gmail.com>
2021-11-04 18:33:48 -0700

    added showInfo button

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ var app = new Vue({
       { id: 0, text: 'Yogurt' },
       { id: 1, text: 'Mango' },
       { id: 2, text: 'Chicken' }
-    ]
+    ],
+    showInfo: false
   },
   methods: {
     reverseMessage: function() {
@@ -21,6 +22,15 @@ var app = new Vue({
     },
     allCaps: function() {
       this.message = this.message.toUpperCase();
+    },
+    toggleInfo: function() {
+      console.log("toggling info");
+      // if (this.showInfo === false) {
+      //   this.showInfo = true;
+      // } else {
+      //   this.showInfo = false;
+      // }
+      this.showInfo = !this.showInfo;
     }
   }
 });
@@ -40,18 +50,3 @@ var app2 = new Vue({
     }
   }
 });
-// Vue.component('todo-item', {
-//   props: ['todo'],
-//   template: '<li>{{ todo.text }}</li>'
-// });
-
-// var app2 = new Vue({
-//   el: '#app-2',
-//   data: {
-//     groceryList: [
-//       { id: 0, text: 'Yogurt' },
-//       { id: 1, text: 'Mango' },
-//       { id: 2, text: 'Chicken' }
-//     ]
-//   }
-// });
